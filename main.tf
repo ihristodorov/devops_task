ffb94c4e411b9becb132e75571f53b68491e6176
ihristodorov <ivailohristodorov@gmail.com>
2020-02-16 22:43:23 +0200

    Add Listener and Security Group with health check for the Load Balancer

--- main.tf
+++ main.tf
@@ -61,6 +61,43 @@ resource "aws_lb" "example" {
 	name	           = "terraform-asg_example"
 	load_balancer_type = "application"
 	subnets		   = data.aws_subnet_ids.default.ids
+	security_groups	   = [aws_security_group.alb.id]
  }
 
+resource "aws_lb_listener" "http" {
+	load_balancer_arn = aws_lb.example.arn
+	port		  = 80
+	protocol	  = "HTTP"
 
+	default_action {
+	 type = "fixed-response"
+	 
+	fixed_response {
+	 content_type = "text/plain"
+	 message_body = "404: page not found"
+	 status_code  = 404
+   }
+  }
+ }
+
+resource "aws_security_group" "alb" {
+	name = "terraform-example-alb"
+
+#Allow inbound HTTP requests
+
+	ingress {
+	 from_port   = 80
+	 to_port     = 80
+	 protocol    = "tcp"
+         cidr_blocks = ["0.0.0.0/0"]
+ }
+
+#Allow all outbound requests
+	
+	egress {
+	 from_port   = 0
+	 to_port     = 0
+	 protocol    = "-1"
+	 cidr_blocks = ["0.0.0.0/0"]
+  }
+ }
