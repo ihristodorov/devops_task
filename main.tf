7fa31ad7b2174c8b1915c3c18542b88b97c600d3
ihristodorov <ivailohristodorov@gmail.com>
2020-02-16 22:57:24 +0200

    Add Load Balancer listener rule

--- main.tf
+++ main.tf
@@ -39,6 +39,10 @@ resource "aws_security_group" "instance" {
 resource "aws_autoscaling_group" "example" {
 	launch_configuration = aws_launch_configuration.example.name
 	vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+
+	target_group_arns = [aws_lb_target_group.asg.arn]
+	health_check_type = "ELB"
+
 	min_size = 2
 	max_size = 10
 
@@ -99,5 +103,36 @@ resource "aws_security_group" "alb" {
 	 to_port     = 0
 	 protocol    = "-1"
 	 cidr_blocks = ["0.0.0.0/0"]
+  }
+ }
+
+resource "aws_lb_target_group" "asg" {
+	name	  = "terraform-asg-example"
+	port	  = var.server_port
+	protocol  = "HTTP"
+	vpc_id    = data.aws_vpc.default.id
+
+	health_check {
+	 path                = "/"
+	 protocol            = "HTTP"
+	 matcher             = "200"
+	 interval            = 15
+	 timeout             = 3
+	 healthy_threshold   = 2
+	 unhealthy_threshold = 2
+  }
+ }
+
+resource "aws_lb_listener_rule" "asg" {
+	listener_arn = aws_lb_listener.http.arn
+	priority     = 100
+
+	condition {
+	 field  = "path-pattern"
+	 values = ["*"]
+  }
+	action {
+	 type		  = "forward"
+	 target_group_arn = aws_lb_target_group.asg.arn
   }
  }
