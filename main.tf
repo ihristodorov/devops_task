b091dccc7103a2e2c3b91083243227aa3e2e26f4
ihristodorov <ivailohristodorov@gmail.com>
2020-02-16 23:11:51 +0200

    Fixing typos

--- main.tf
+++ main.tf
@@ -63,7 +63,7 @@ data "aws_subnet_ids" "default" {
 
 
 resource "aws_lb" "example" {
-	name	           = "terraform-asg_example"
+	name	           = "terraform-asg-example"
 	load_balancer_type = "application"
 	subnets		   = data.aws_subnet_ids.default.ids
 	security_groups	   = [aws_security_group.alb.id]
