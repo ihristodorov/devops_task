d140e6a0d0ce11f298cdf2db4080ce3dd34abd4e
ihristodorov <ivailohristodorov@gmail.com>
2020-02-16 21:21:15 +0200

    Creating AWS launch configuration; Creating a security group in order EC2 instance to receive traffc; Creating a variable for the server port; Creating a web server and with the help of busybox to start it up.

--- main.tf
+++ main.tf
@@ -1,3 +1,37 @@
 provider "aws" {
 	region  = "eu-central-1"
  }
+
+
+resource "aws_launch_configuration" "example" {
+	image_id	      = "ami-0b418580298265d5c"
+	instance_type 	      = "t2.micro"
+	security_groups	      = [aws_security_group.instance.id]
+
+	user_data = <<-EOF
+		 #!/bin/bash
+		 echo "Hello, This is a simple Web Server" > index.html
+		 nohup busybox httpd -f -p ${var.server_port} &
+		 EOF
+
+	lifecycle {
+		 create_before_destroy = true
+  } 
+ }
+
+variable "server_port" {
+	description = "The port the server will use for HTTP requests"
+	type	    = number
+	default	    = 8080
+ }
+
+resource "aws_security_group" "instance" {
+	name = terraform-example-instance"
+
+	ingress {
+	 from_port   = var.server_port
+	 to_port     = var.server_port
+	 protocol    = "tcp"
+	 cidr_blocks = ["0.0.0.0/0"]
+  }
+ }
